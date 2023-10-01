5b0023f35abaa9338b3709122c08b9e4e41aa943
MikhailRuzmanov <mihailruzmanov@mail.ru>
2023-10-01 09:16:09 +0300

    Домашняя работа №2.4 готово

--- src/main/java/pro/sky/skyprospringcalculator/CalculatorService.java
+++ src/main/java/pro/sky/skyprospringcalculator/CalculatorService.java
@@ -1,3 +1,33 @@
 package pro.sky.skyprospringcalculator;
 
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public String welcome(){
+        return "Добро пожаловать в калькулятор";
+    }
+    public String answerWelcome(String userName){
+        return "Добро пожаловать в калькулятор " + userName;
+    }
+    public String plusCalculator(int num1, int num2) {
+        return String.valueOf((num1 + num2));
+    }
+
+    public String minusCalculator(int num1, int num2) {
+        return String.valueOf((num1 - num2));
+    }
+
+    public String multiplyCalculator(int num1, int num2) {
+        return String.valueOf((num1 * num2));
+    }
+
+    public String divideCalculator(int num1, int num2) {
+        if (num2 == 0) {
+            return "Делить на 0 нельзя";
+        } else {
+            return String.valueOf((float) (num1 / num2));
+        }
+    }
+}
 
